d249159d50126c854a9ec9911b95e33b248c4888
Kim, Joo-Ho <teamsprint@gmail.com>
2017-05-29 17:01:16 +0900

    before log4jdbc

---
+++ src/main/java/org/zerock/web/SampleController4.java
@@ -0,0 +1,14 @@
+package org.zerock.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController4 {
+	@RequestMapping("/doJSON")
+	public @ResponseBody ProductVO doJSON() {
+		ProductVO vo = new ProductVO("샘플상품", 30000);
+		return vo;
+	}
+}
